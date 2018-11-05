91b5afe9242d633e3af3d95279f9eb3dcb32107b
Graham Hindle <grahamhindle@mac.com>
2018-11-05 01:09:01 +0100

    deconstructed props

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -2,39 +2,39 @@ import React,{Component} from 'react'
 import './App.css';
 import PropTypes from 'prop-types'
 import Book from './Book'
+import {Link} from 'react-router-dom'
 
 class BookSearch extends Component {
   static propTypes = {
     books: PropTypes.array.isRequired,
     onChangeBookStatus: PropTypes.func.isRequired,
+    getQueryResults: PropTypes.func.isRequired,
+    updateQuery: PropTypes.func.isRequired,
   }
   
-    
   handleSubmit = ((e)=>{
     e.preventDefault();
-    //in App Component
     this.props.getQueryResults()
-
   })
     
-updateQuery=((query)=>{
-  this.props.updateQuery(query)
-})
-  
+  updateQuery=((query)=>{
+    this.props.updateQuery(query)
+  })
+    
   onShelfChange = ((book)=> {
     this.props.onChangeBookStatus(book)
-  })
-
-  
+    })
 
   render() {
-
-     
+    const {query, searchResults}= this.props;
     return (
       <div>
         <form onSubmit = {this.handleSubmit}>
           <div className="search-books">
             <div className="search-books-bar">
+            <Link to = '/' className = "close-search">
+              Close
+            </Link>
               <div className="search-books-input-wrapper">
               <input type="text" 
                 value={this.props.query} 
@@ -43,18 +43,15 @@ updateQuery=((query)=>{
               </div>
             </div>
           </div>  
-          
-          
           <div className="search-books-results">
             <div className="showing-contacts">
-              
-              {this.props.query !== ''? this.props.searchResults.length  === 0 ? 
-              <span>{`No results for query: ${this.props.query}`}</span> :
-              <span>{`There are ${this.props.searchResults.length} results for this Search: ${this.props.query}` }</span>
+              {this.props.query !== ''? searchResults.length  === 0 ? 
+              <span>{`No results for query: ${query}`}</span> :
+              <span>{`There are ${searchResults.length} results for this Search: ${query}` }</span>
               : <span></span>}
           </div>
           <ol className="books-grid">
-                {this.props.searchResults.map((book)=>(
+                {searchResults.map((book)=>(
                 <Book key = {book.id} 
                   book={book} 
                   updateQuery={this.updateQuery}
