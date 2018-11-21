61d62c1566a85dbc5c0957032dee1bb913670bf4
Graham Hindle <grahamhindle@mac.com>
2018-11-22 00:54:35 +0100

    added book update to storage to change the books shelf status

--- src/App.js
+++ src/App.js
@@ -1,88 +1,98 @@
-import React, { Component } from 'react';
-import './App.css';
-import MyReads from './MyReads'
-import BookSearch from './BookSearch'
-import  * as BooksAPI from './BooksAPI'
-import {Route} from 'react-router-dom'
+import React, { Component } from "react";
+import "./App.css";
+import MyReads from "./MyReads";
+import BookSearch from "./BookSearch";
+import * as BooksAPI from "./BooksAPI";
+import { Route } from "react-router-dom";
 
 class App extends Component {
-  constructor(props){
-    super(props)
-    this.state={
-      books:[],
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: []
+    };
   }
-  
-  componentDidMount(){
-   //get ther initial books data
-    BooksAPI.getAll()
-    .then((books)=>{
-      this.setState(()=> ({
-        books
-      }))
-    })
-  } 
 
-  
+  componentDidMount() {
+    //get ther initial books data
+    BooksAPI.getAll().then(books => {
+      this.setState(() => ({
+        books
+      }));
+    });
+  }
+
+  bookSave(book, shelf) {
+    //save the current state of books
+    BooksAPI.update(book, shelf);
+  }
+
   /**************************************************
   Update Book PATH
   change the status of a book / move the shelf it is
   on
   ***************************************************/
-  
-  updateStatus = ((book)=>{
+
+  updateStatus = book => {
     //update the status in the shelf
-    let newBooks = this.state.books.map(b =>{
-      if ( b.id !== book.id )return b
-        return {
-          ...b,
-          shelf :  book.shelf 
-        } 
-    }) 
+    let newBooks = this.state.books.map(b => {
+      if (b.id !== book.id) return b;
+      this.bookSave(book, book.shelf);
+      return {
+        ...b,
+        shelf: book.shelf
+      };
+    });
     //filter out the books that have status of none in bookshelf
     //and update state
-    newBooks = newBooks.filter(b => b.shelf !== 'none')
-    this.setState(()=> ({
+    newBooks = newBooks.filter(b => b.shelf !== "none");
+    this.setState(() => ({
       books: newBooks
-    }))
-    
-  })
+    }));
+  };
 
- 
-/**********************************************************
+  /**********************************************************
   add book from search window and update books
 ************************************************************/
-  bookMove = ((book)=>{
+  bookMove = book => {
     //add book to books
-    console.log('app.js',book.shelf)
-    let newBooks = this.state.books
-    let c = newBooks.concat(book) 
-    this.setState(()=> ({
+    console.log("app.js", book.shelf);
+    let newBooks = this.state.books;
+    let c = newBooks.concat(book);
+    this.setState(() => ({
       books: c
-    }))
-  })
-  
+    }));
+    this.bookSave(book, book.shelf);
+  };
+
   /********************************************************
    render
   *********************************************************/
   render() {
-    return(
-    <div>
-      <Route exact path = '/' render={() => (
-        <MyReads
-          books={this.state.books}
-          onChangeBookStatus = {this.updateStatus}
+    return (
+      <div>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <MyReads
+              books={this.state.books}
+              onChangeBookStatus={this.updateStatus}
+            />
+          )}
         />
-      )}/>
-      <Route path = '/booksearch' render={()=>(
-        <BookSearch
-          books={this.state.books}
-          bookMove = {this.bookMove}
-          onChangeBookStatus = {this.bookMove}
-          />
-      )}/>
-    </div>
-    )
+        <Route
+          path="/booksearch"
+          render={() => (
+            <BookSearch
+              books={this.state.books}
+              bookMove={this.bookMove}
+              onChangeBookStatus={this.bookMove}
+            />
+          )}
+        />
+      </div>
+    );
   }
 }
 
