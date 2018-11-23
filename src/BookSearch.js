50468e3e2977366d1cfb43713eec4ae36e15fd43
Graham Hindle <grahamhindle@mac.com>
2018-11-23 06:49:44 +0100

    search changes

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -1,136 +1,157 @@
-import React,{Component} from 'react'
-import './App.css';
-import PropTypes from 'prop-types'
-import Book from './Book'
-import {Link} from 'react-router-dom'
-import  * as BooksAPI from './BooksAPI'
+import React, { Component } from "react";
+import "./App.css";
+import PropTypes from "prop-types";
+import Book from "./Book";
+import { Link } from "react-router-dom";
+import * as BooksAPI from "./BooksAPI";
 
 class BookSearch extends Component {
-  constructor (props){
-    super(props)
-    this.state = 
-    {
-      queryString: '',
+  constructor(props) {
+    super(props);
+    this.state = {
+      queryString: "",
       bookSearchResults: []
-    }
+    };
 
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
   static propTypes = {
     books: PropTypes.array.isRequired,
-    onChangeBookStatus: PropTypes.func.isRequired,
-  }
+    onChangeBookStatus: PropTypes.func.isRequired
+  };
 
   handleChange = () => {
-    this.setState({bookSearchResults: []})
-    this.setState({queryString: this.search.value}
-      ,()=>{
-        if( this.state.queryString.length > 0){
-            this.queryResults()
-        }
-      })
-  }
-  
-  handleSubmit (event){
-    event.preventDefault();
-    this.handleChange()
-  }
-    
+    this.setState({ bookSearchResults: [] });
+    this.setState({ queryString: this.search.value }, () => {
+      if (this.state.queryString.length > 0) {
+        this.queryResults();
+      }
+    });
+  };
 
-/****************************************************
+  handleSubmit(event) {
+    event.preventDefault();
+    this.handleChange();
+  }
+
+  /****************************************************
   SEARCH PATH - Query from backend server
 ****************************************************/
- queryResults=(()=>{
-    BooksAPI.search(this.state.queryString)
-    .then((queryResult )=>{
-    this.createSearchResults(queryResult) 
-    })
-})
+  queryResults = () => {
+    BooksAPI.search(this.state.queryString).then(queryResult => {
+      this.createSearchResults(queryResult);
+    });
+  };
 
-
-/**********************************************************
+  /**********************************************************
   SEARCH PATH
   helper function to filter out books that we already have on 
   our bookshelves after initial search load 
   and set shelf status to none
-************************************************************/  
- createSearchResults = ((res)=> {
-  if(res.length > 0 ){
-    //is the book already in our shelves ?
-    const newBooks = res
-    .filter(o => !this.props.books
-    .find(o2 => o.id === o2.id))
-    newBooks.map(book => book.shelf = "none")
-    this.setState(()=> ({
-      bookSearchResults: newBooks
-    }))
-  }
-  else {
-   this.setState(()=> ({
-      bookSearchResults: []
-    }))
-  }
-})
+************************************************************/
 
-  
-  onShelfChange = ((book)=> {
-    this.bookMove(book)
-  })
+  createSearchResults = res => {
+    if (res.length > 0) {
+      const newRes = res.map(val => {
+        const b = this.props.books.find(() => {
+          val.id === b.id ? (val.shelf = b.shelf) : (val.shelf = "None");
+          console.log(val);
+        });
+      });
+      this.setState(() => ({
+        bookSearchResults: newRes
+      }));
+      //is the book already in our shelves ?
+
+      /*
+      res.forEach(
+        o =>
+          (const result = this.props.books.find(o2 => {
+            if (result.id === o2.id) {
+              console.log("result", result.id);
+              console.log("books", o2);
+              o.shelf = o2.shelf;
+            }
+          }))
+      );
+      this.setState(() => ({
+        bookSearchResults: res
+      }));
+      */
+    } else {
+      this.setState(() => ({
+        bookSearchResults: []
+      }));
+    }
+  };
+
+  onShelfChange = book => {
+    this.bookMove(book);
+  };
 
   /**********************************************************
   SEARCH PATH
   move book from search window and update books
   ************************************************************/
-  bookMove = ((book)=>{
+  bookMove = book => {
     //add book to books
-    this.props.bookMove(book)
+    this.props.bookMove(book);
     //remove from querylist
-    const newSearchList = this.state.bookSearchResults
-      .filter(b => b.id !== book.id)
-    this.setState(()=> ({
+    const newSearchList = this.state.bookSearchResults.filter(
+      b => b.id !== book.id
+    );
+    this.setState(() => ({
       bookSearchResults: newSearchList
-    }))
-  })
-
+    }));
+  };
 
   render() {
-    const { bookSearchResults, queryString}= this.state;
+    const { bookSearchResults, queryString } = this.state;
     return (
       <div>
-        <form onSubmit = {this.handleSubmit}>
+        <form onSubmit={this.handleSubmit}>
           <div className="search-books">
             <div className="search-books-bar">
-            <Link to = '/' className = "close-search">
-              Close
-            </Link>
+              <Link to="/" className="close-search">
+                Close
+              </Link>
               <div className="search-books-input-wrapper">
-              <input
-                ref = {input => this.search = input}
-                placeholder="Search by title or author"
-                onChange = {this.handleChange}/>
+                <input
+                  ref={input => (this.search = input)}
+                  placeholder="Search by title or author"
+                  onChange={this.handleChange}
+                />
               </div>
             </div>
-          </div>  
+          </div>
           <div className="search-books-results">
             <div className="showing-contacts">
-              {this.props.query !== ''? bookSearchResults.length  === 0 ? 
-              <span>{`No results for query: ${queryString}`}</span> :
-              <span>{`There are ${bookSearchResults.length} results for this Search: ${queryString}` }</span>
-              : <span></span>}
-          </div>
-          <ol className="books-grid">
-                {bookSearchResults.map((book)=>(
-                <Book key = {book.id} 
-                  book={book} 
-                  changeShelf = {this.onShelfChange}/>
+              {this.props.query !== "" ? (
+                bookSearchResults.length === 0 ? (
+                  <span>{`No results for query: ${queryString}`}</span>
+                ) : (
+                  <span>{`There are ${
+                    bookSearchResults.length
+                  } results for this Search: ${queryString}`}</span>
+                )
+              ) : (
+                <span />
+              )}
+            </div>
+            <ol className="books-grid">
+              {bookSearchResults.map(book => (
+                <Book
+                  key={book.id}
+                  book={book}
+                  changeShelf={this.onShelfChange}
+                />
               ))}
-          </ol>
+            </ol>
           </div>
         </form>
       </div>
-      
-    )
+    );
   }
 }
-export default BookSearch
+export default BookSearch;
